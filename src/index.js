e61224f764716ee7ac0cdb639c46024c94af9406
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-08-23 18:31:50 +0200

    Lists

--- src/index.js
+++ src/index.js
@@ -5,23 +5,30 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const firstData = {
-	title: 'Necessary Trouble: Growing Up at Midcentury',
-	author: 'Drew Gilpin Faust',
-	img: 'https://images-na.ssl-images-amazon.com/images/I/81LGc69gzQL._AC_UL600_SR600,400_.jpg',
-};
-
-const secondData = {
-	title: 'The Four Agreements: A Practical Guide to Personal Freedom',
-	author: 'Don Miguel Ruiz',
-	img: 'https://m.media-amazon.com/images/I/51EA+bXwJsL._SX343_BO1,204,203,200_.jpg',
-};
+const books = [
+	{
+		author: 'Keila Shaheen',
+		title: 'The Shadow Work Journal',
+		img: 'https://m.media-amazon.com/images/I/31UAsu2Q94L._SX331_BO1,204,203,200_.jpg',
+	},
+	{
+		author: 'James Clear',
+		title: 'Atomic Habits',
+		img: 'https://m.media-amazon.com/images/I/51B7kuFwQFL._SX329_BO1,204,203,200_.jpg',
+	},
+	{
+		author: 'Ann Patchett',
+		title: 'Tom Lake',
+		img: 'https://m.media-amazon.com/images/I/51F4TgwRlyL._SX329_BO1,204,203,200_.jpg',
+	},
+];
 
 const BookList = () => {
 	return (
 		<section className='booklist'>
-			<Book data={firstData} />
-			<Book data={secondData} />
+			{books.map((book) => (
+				<Book data={book} />
+			))}
 		</section>
 	);
 };
