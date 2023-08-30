f47ce93595edea3e80b82f921bd940fe8f2b3b66
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-08-30 10:58:40 +0200

    Event Object

--- src/components/EventExamples.jsx
+++ src/components/EventExamples.jsx
@@ -1,5 +1,8 @@
 const EventExamples = () => {
 	const handleFormInput = (e) => {
+		console.log(e.target);
+		console.log(e.target.name);
+		console.log(e.target.value);
 		console.log('Handle form input');
 	};
 
@@ -7,13 +10,23 @@ const EventExamples = () => {
 		alert('Handle button click');
 	};
 
+	const handleFormSubmission = (e) => {
+		e.preventDefault();
+		console.log('form');
+	};
+
 	return (
 		<section>
-			<form>
+			<form onSubmit={handleFormSubmission}>
 				<h2>Typical Form</h2>
 				<input onChange={handleFormInput} type='text' name='example' style={{ marginBlock: '1rem' }} />
+				<button type='submit'>Submit</button>
+				<div>
+					<button type='button' onClick={handleButtonClick}>
+						Click Me
+					</button>
+				</div>
 			</form>
-			<button onClick={handleButtonClick}>Click Me</button>
 		</section>
 	);
 };
